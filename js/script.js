fad61abf389c23d81e76ca86843299142e096028
Anatolii Bedilo <fides2013ml@gmail.com>
2022-03-16 20:37:21 +0300

    added new slider

--- js/script.js
+++ js/script.js
@@ -5,23 +5,48 @@ document.getElementById("burger-menu").onclick = (event) => {
   document.getElementsByTagName('body')[0].classList.toggle('hidden')
 }
 
-const swiper = new Swiper('.swiper', {
-  observer: true,
-  observerParents: true,
-  slidesPerView: 1,
-  spaceBetween: 32,
-  watchOverflow: true,
-  speed: 1200,
-  loop: true,
-  loopAdditionalSlides: 5,
-  preloadImages: false,
-  parallax: true,
-  autoplay: true,
-  pagination: {
-    el: '.swiper-pagination',
-  },
-  navigation: {
-    nextEl: '.swiper .slider-arrows-next',
-    prevEl: '.swiper .slider-arrows-prev',
-  },
-})
+if (document.querySelector('.slider-main-ukrain')){
+  const swiperUkrain = new Swiper('.slider-main-ukrain', {
+    observer: true,
+    observerParents: true,
+    slidesPerView: 1,
+    spaceBetween: 32,
+    watchOverflow: true,
+    speed: 1200,
+    loop: true,
+    loopAdditionalSlides: 5,
+    preloadImages: false,
+    parallax: true,
+    autoplay: true,
+    pagination: {
+      el: '.swiper-pagination',
+    },
+    navigation: {
+      nextEl: '.slider-main-ukrain .slider-arrows-next',
+      prevEl: '.slider-main-ukrain .slider-arrows-prev',
+    },
+  })
+}
+
+if (document.querySelector('.slider-main-building')){
+  const swiperBuilding = new Swiper('.slider-main-building', {
+    observer: true,
+    observerParents: true,
+    slidesPerView: 1,
+    spaceBetween: 32,
+    watchOverflow: true,
+    speed: 1200,
+    loop: true,
+    loopAdditionalSlides: 5,
+    preloadImages: false,
+    parallax: true,
+    // autoplay: true,
+    // pagination: {
+    //   el: '.swiper-pagination',
+    // },
+    navigation: {
+      nextEl: '.building-arrows__next',
+      prevEl: '.building-arrows__prev',
+    },
+  })
+}
