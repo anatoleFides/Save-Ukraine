7a932a6efcb0a764e18b5e260bae9557815a6749
Anatolii Bedilo <fides2013ml@gmail.com>
2022-03-20 19:48:19 +0300

    added form radio fron donation. Added mobile version from donation

--- how-can-help/js/script.js
+++ how-can-help/js/script.js
@@ -9,35 +9,13 @@ function include (url) {
 include('../js/all-script.js')
 
 //Tabs
-// $(document).ready(function(){
-// 	$('.nav-tabs__link').on('click', function(event){
-// 		event.preventDefault();
-
-// 	var
-// 		itemNav = $(this).closest('.nav-tabs__item'),
-// 		itemPage = $('.tabs-donation__page'),
-// 		itemPosition = itemNav.index();
-
-// 	itemPage.eq(itemPosition)
-// 		.add(itemNav)
-// 		.addClass('tabs-active')
-// 		.siblings()
-// 		.removeClass('tabs-active');
-// 	});
-// });
-
-
-
 const showPage = (event, numberPage) => {
-
   const navTabLinks = document.querySelectorAll('.nav-tabs__item')
 
   for (let item of navTabLinks) {
     item.classList.remove('tabs-active')
   }
   const pages = document.querySelectorAll('.tabs-donation__page')
-
-  console.log(pages)
 
   for (let page of pages) {
     page.classList.remove('tabs-active')
