1a87b0c9014d9042cedef8200fc2d34f3416ff78
Anatolii Bedilo <fides2013ml@gmail.com>
2022-03-21 00:18:00 +0300

    added script insert sum from input

--- how-can-help/js/script.js
+++ how-can-help/js/script.js
@@ -25,21 +25,48 @@ const showPage = (event, numberPage) => {
   event.target.classList.add('tabs-active')
 }
 
-//Input
+//Check Input
 const checkValue = (event, btnId) => {
-  const inputs = document.querySelectorAll('.input-text__custom')
+  let inputValue = parseFloat(event.target.value) || 0
 
-  for (let input of inputs) {
-    let inputValue = parseFloat(event.target.value) || 0
+  if (inputValue > 0) {
+    document.getElementById(btnId).disabled = false
+    event.target.nextElementSibling.innerText = ''
+  } else {
+    document.getElementById(btnId).disabled = true
+    event.target.nextElementSibling.innerText = 'Невірний формат суми'
+  }
 
-    if (inputValue > 0) {
-      document.getElementById(btnId).disabled = false
-    } else {
-      document.getElementById(btnId).disabled = true
-      console.log(event.target)
-      event.target.innerContent = 'Невірний формат суми'
+  (event.target.value === '') && (event.target.nextElementSibling.innerText = '')
+}
+
+//Insert sum
+const sumListOfCard = document.getElementsByClassName('price-card')
+
+for (let sumOfCard of sumListOfCard) {
+  sumOfCard.onclick = (event) => {
+    for (let sumOfCard of sumListOfCard) {
+      sumOfCard.classList.remove('.sum-active')
     }
+
+    event.target.classList.add('.sum-active')
+    const sumValueofCard = event.target.innerText
+
+    document.getElementById('price-card').setAttribute("value", sumValueofCard)
   }
 }
 
+const sumListOfRequisites = document.getElementsByClassName('pay-requisites')
 
+for (let sumOfRequisites of sumListOfRequisites) {
+  sumOfRequisites.onclick = (event) => {
+    for (let sumOfRequisites of sumListOfRequisites) {
+      sumOfRequisites.classList.remove('.sum-active')
+    }
+
+    event.target.classList.add('.sum-active')
+    const sumValueofRequisites = event.target.innerText
+
+    document.getElementById('pay-requisites').setAttribute("value", sumValueofRequisites)
+  }
+}
