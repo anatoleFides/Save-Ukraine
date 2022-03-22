91a09a82e7657c0069d1fba68194cb18bd325ce4
Anatolii Bedilo <fides2013ml@gmail.com>
2022-03-22 20:24:24 +0300

    added flag-gig. Started create anchor top

--- js/all-script.js
+++ js/all-script.js
@@ -38,3 +38,38 @@ for (let i = 0; i < arrows.length; i++) {
     } 
   }
 }
+
+//Anchor to top
+// document.getElementById('anchor-top').onclick = (event) => {
+//   function up () {
+//     let top = Math.max(document.body.scrollTop, document.documentElement.scrollTop)
+
+//     if(top > 0) {
+//      window.scrollBy(0, ((top+100)/-10))
+
+//       const t = setTimeout(() => up(), 20)
+//     } else clearTimeout(t)
+//       return false
+//     }
+// }
+
+$(document).ready(function(){
+       $('#scroller').hide();  
+      //fade in/out based on scrollTop value
+      $(window).scroll(function() {
+        if ($(window).scrollTop() > 100) {
+          $('#scroller').fadeIn();
+        }else{
+          $('#scroller').fadeOut();
+          // alert ('hello word');
+        }
+      });
+     
+      // scroll body to 0px on click
+      $('#scroller').click(function () {
+        $('body,html').animate({
+          scrollTop: 0
+        }, 400);
+        return false;
+      });
+    });
