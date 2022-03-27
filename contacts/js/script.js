3eda1433ebb8ac6795da9488b32b32c3785bfb29
Anatolii Bedilo <fides2013ml@gmail.com>
2022-03-27 12:05:49 +0300

    added google map

--- contacts/js/script.js
+++ contacts/js/script.js
@@ -7,3 +7,63 @@ const include = (url) => {
 }
 
 include('../js/all-script.js')
+
+function initMap () {
+  let pos = {lat: 49.2260640, lng:28.4059510}
+  let myElem = document.getElementById('map')
+  let myOptions = {
+    zoom: 16,
+    center: pos,
+    styles: [
+    {
+        "stylers": [
+            {
+                "hue": "#007fff"
+            },
+            {
+                "saturation": 89
+            }
+        ]
+    },
+    {
+        "featureType": "water",
+        "stylers": [
+            {
+                "color": "#ffffff"
+            }
+        ]
+    },
+    {
+        "featureType": "administrative.country",
+        "elementType": "labels",
+        "stylers": [
+            {
+                "visibility": "off"
+            }
+        ]
+    }
+]
+  }
+
+  const myMap = new google.maps.Map(myElem, myOptions)
+
+  const marker = new google.maps.Marker({
+    position: pos,
+    map: myMap,
+    title:'БФ "Врятувати Україну"',
+    icon: 'image/Marker.svg',
+    animation: google.maps.Animation.BOUNCE
+  })
+//   function addMarker(properties){
+
+//     marker.setIcon("../image/Marker.svg");
+
+// 		// var infowindow = new google.maps.InfoWindow({
+// 		// 	content: properties.info,
+// 		// 	maxWidth: 135
+// 		// })
+//   //   marker.addListener('click', function() {
+// 		// 	infowindow.open(myMap, marker);
+// 		// })
+//   }
+}
