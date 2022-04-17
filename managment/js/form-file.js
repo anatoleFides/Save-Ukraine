490f9721880b69968856694ca799ad91ed3265e6
Anatolii Bedilo <fides2013ml@gmail.com>
2022-04-17 19:37:50 +0300

    added validete input in manager page

--- managment/js/form-file.js
+++ managment/js/form-file.js
@@ -1,20 +1,56 @@
+const messages = [
+  'Не може бути порожнім',
+  'Невірний формат зображення',
+  'Розміз зображення вище 1МВ',
+  ''
+]
+
+const showMessage = (elem, index) => Object.assign(elem.nextElementSibling, {
+  textContent: messages[index]
+})
+
+const inputs = document.querySelectorAll('.input-text__custom')
+
+for (let input of inputs) {
+
+  input.oninput = (event) => {
+
+    !input.value ? showMessage(input, 0) : showMessage(input, 3)
+  }
+}
 const inputFile = document.getElementById('txtImages')
-const containerFiles = document.getElementById('file-container')
 
 inputFile.onchange = (event) => {
-  console.log(event.target.files[0])
 
   const listFiles = event.target.files
 
   for (let file of listFiles) {
-    let elemContainer = document.createElement('div')
-    elemContainer.innerHTML = `
-      <div class="container-item">
-      <p class="file-item">${file.name} <span>${Math.round(file.size/1000)}kB</span></p>
-        <p class="delete-file"></p>
-      </div>
-    `
-    containerFiles.appendChild(elemContainer)
+
+    if (file.type.indexOf('image')) {
+
+      showMessage(event.target, 1)
+
+    } else {
+
+      showMessage(event.target, 3)
+
+      if (file.size > 1000000) {
+
+        showMessage(event.target, 2)
+      } else {
+
+        let elemContainer = document.createElement('div')
+
+        elemContainer.innerHTML = `
+          <div class="container-item">
+            <p class="file-item">${file.name} <span>${Math.round(file.size/1000)}kB</span></p>
+            <p class="delete-file"></p>
+          </div>
+        `
+        document.getElementById('file-container')
+          .appendChild(elemContainer)
+      }
+    }
   }
 
   const buttonsDeleteFile = document.querySelectorAll('.delete-file')
